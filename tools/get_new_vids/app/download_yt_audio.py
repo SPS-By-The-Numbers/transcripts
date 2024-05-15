dfc9dfecca83ceb4c800c9d6a4e742e0610a15ae
Albert J. Wong <awong.dev@gmail.com>
2024-05-15 00:00:17 -0700

    Clean up get_new_vids docker setup.

--- tools/get_new_vids/app/download_yt_audio.py
+++ tools/get_new_vids/app/download_yt_audio.py
@@ -18,6 +18,16 @@
 #py download_yt_audio.py -o "C:\Users\Joseph\Documents\test_out" -u "https://www.youtube.com/user/wanderbots/"
 #note the user/ instead of @, since @wanderbots would break with pytube
 
+# List the same URL structure regex that pytube uses for checking if a url
+# is a channel or a playlist.
+CHANNEL_URL_PATTERNS = [
+    r"(?:\/(c)\/([%\d\w_\-]+)(\/.*)?)",
+    r"(?:\/(channel)\/([%\w\d_\-]+)(\/.*)?)",
+    r"(?:\/(u)\/([%\d\w_\-]+)(\/.*)?)",
+    r"(?:\/(user)\/([%\w\d_\-]+)(\/.*)?)"
+]
+CHANNEL_URL_REGEXP = re.compile('|'.join(CHANNEL_URL_PATTERNS))
+
 from pytube import YouTube
 from pytube import Playlist
 from pytube import Channel
@@ -37,45 +47,17 @@ def get_outfile_base(outdir, video_id):
     return os.path.join(outdir, video_id[0], video_id)
 
 
-def is_json_file(path):
-    """Returns True if path exists and can be loaded as JSON."""
-    try:
-        with open(path, "r") as f:
-            json.load(f)
-            return True
-    except:
-            return False
-
 def get_videos(url) -> str:
-    
-    if not url:
-        sys.exit(1)
-    
     videos = get_video_urls(url)
-    vidIDs = [vid.split('=')[1] for vid in videos]
-    return vidIDs
+    vid_ids = [vid.split('=')[1] for vid in videos]
+    return vid_ids
+
 
 def get_video_urls(url):
-    # Check URL structure against the smae regex pytube uses for checking if a url is a channel, otherwise, assume it's a playlist
-    patterns = [
-        r"(?:\/(c)\/([%\d\w_\-]+)(\/.*)?)",
-        r"(?:\/(channel)\/([%\w\d_\-]+)(\/.*)?)",
-        r"(?:\/(u)\/([%\d\w_\-]+)(\/.*)?)",
-        r"(?:\/(user)\/([%\w\d_\-]+)(\/.*)?)"
-    ]
-    isChannelURL = False;
-    for pattern in patterns:
-        regex = re.compile(pattern)
-        function_match = regex.search(url)
-        if function_match:
-            isChannelURL = True;
-            break
-    if isChannelURL:
-        videos = Channel(url).video_urls
-    else:
-        videos = Playlist(url).videos
-    
-    return videos
+    if CHANNEL_URL_REGEXP.match(url):
+        return Channel(url).video_urls
+    return Playlist(url).videos
+
 
 if(__name__ == "__main__"):
     main()
