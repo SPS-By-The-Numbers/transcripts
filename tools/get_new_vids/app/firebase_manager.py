b031c39c4ef36b78a13d26867c8331efb0126782
Albert J. Wong <awong.dev@gmail.com>
2024-05-15 00:00:17 -0700

    Clean up get_new_vids docker setup.

--- tools/get_new_vids/app/firebase_manager.py
+++ tools/get_new_vids/app/firebase_manager.py
@@ -1,16 +1,13 @@
+import os
+
 import firebase_admin
-from firebase_admin import credentials
 from firebase_admin import db
 
-# Fetch the service account key JSON file contents
-cred = credentials.Certificate('/usr/src/app/hello-web-410622-firebase-adminsdk-85y84-be3b1fe5be.json')
-database = {'databaseURL': 'https://hello-web-410622-default-rtdb.firebaseio.com'}
-
-# Initialize the app with a service account, granting admin privileges
-firebase_admin.initialize_app(cred, database)
+# Initialize app from environment.
+firebase_admin.initialize_app()
 
 # As an admin, the app has access to read and write all data, regradless of Security Rules
-queueRef = db.reference('/transcripts/backend/queue/new');
+new_vid_q = db.reference('/private/transcripts/new-vid-q');
 
 def queue_video_data(videos):
     completed = get_completed()
