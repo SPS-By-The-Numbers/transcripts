e73a7357cebd653d14900dc745336a7d2a7939b9
Albert J. Wong <awong.dev@gmail.com>
2024-05-19 01:08:45 -0700

    add in Vast Instance deregistration

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 import { speakerinfo } from './speakerinfo.js';
 import { metadata, transcript, start_transcribe } from './transcript.js';
-import { video_queue } from './video_queue.js';
+import { video_queue, vast } from './video_queue.js';
 
 import { initializeFirebase } from './firebase_utils.js';
 
 initializeFirebase();
 
-export { speakerinfo, metadata, start_transcribe, transcript, video_queue };
+export { speakerinfo, metadata, start_transcribe, transcript, video_queue, vast };
