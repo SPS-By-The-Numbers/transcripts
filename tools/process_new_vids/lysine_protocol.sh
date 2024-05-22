6122d513e26de8d59213920b4186b963d6eaa6dd
Albert J. Wong <awong.dev@gmail.com>
2024-05-22 00:12:59 -0700

    Fix math in lysine_protocol for minutes,

--- tools/process_new_vids/lysine_protocol.sh
+++ tools/process_new_vids/lysine_protocol.sh
@@ -3,7 +3,7 @@
 # Stops the container if it's been runnig for too long.
 
 # Sleep for 2 hours.
-sleep $((${1:-1} * 60 * 1000))
+sleep $((${1:-120} * 60))
 
 # Stops the container if the script takes longer than two hours, and won't
 # execute if the script finishes on time (<2 hours)
