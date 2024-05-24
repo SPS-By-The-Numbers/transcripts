744fa1fc09e4b9d0e38b9554b1a5f0a374fbf67d
Albert J. Wong <awong.dev@gmail.com>
2024-05-24 00:25:10 -0700

    Allow cluster of machines to handle backlog of videos

--- functions-python/main.py
+++ functions-python/main.py
@@ -10,32 +10,42 @@ app = initialize_app()
 
 DISK_GB=25
 INSTANCE_LABEL='auto_transcribe'
+MINS_PER_VID = 10
+VIDS_PER_MACHINE = 20
 
 @pubsub_fn.on_message_published(topic="start_transcribe", region="us-west1")
 def start_transcribe(event: pubsub_fn.CloudEvent[pubsub_fn.MessagePublishedData]) -> None:
     """Start the vast.ai instance which queries the new videos"""
-    print("Message received")
-    # Get list of videos.
-    for category in ["sps-board"]:
+
+    # See if there are videos
+    num_new_videos = 0
+    for category in ["sps-board", "seattle-city-council"]:
         print(f"Examining video queue for in {category}")
         new_vids_queue = db.reference(f"/transcripts/private/{category}/new_vids").get(False, True)
 
-        # If there are any entries, ensure a vast.ai instance is running.
-        if len(new_vids_queue):
+        if new_vids_queue:
             print(f"Found {len(new_vids_queue)} videos for {category}")
-            # TODO: heartbeat terminate stale instances.
+            num_new_videos = num_new_videos + len(new_vids_queue)
 
-            # Search for all offers.
-            # TODO: Retry loop.
-            vast = VastAI('14d2615319906234466013432944c5049f7b90829e72f414a0ca35f2300f3cb0', raw=True)
+    # If there are any entries, ensure a vast.ai instance is running.
+    if num_new_videos > 0:
+        # TODO: heartbeat terminate stale instances.
+        lysine_timeout = num_new_videos * MINS_PER_VID
 
-            # Do not create new instance if one is running.
-            all_instances = json.loads(vast.show_instances())
-            running_transcribers = [x for x in all_instances if x['label'] == INSTANCE_LABEL]
-            if running_transcribers:
-                print(f"Instances already running {repr([x['id'] for x in running_transcribers])}.")
-                break
+        # Search for all offers.
+        # TODO: Retry loop.
+        vast = VastAI('14d2615319906234466013432944c5049f7b90829e72f414a0ca35f2300f3cb0', raw=True)
 
+        target_num_instances = int(num_new_videos / VIDS_PER_MACHINE)
+
+        # Do not create new instance if one is running.
+        all_instances = json.loads(vast.show_instances())
+        running_transcribers = [x for x in all_instances if x['label'] == INSTANCE_LABEL]
+        if len(running_transcribers) >= target_num_instances:
+            print(f"Instances already running {repr([x['id'] for x in running_transcribers])}.")
+            return
+
+        for _ in range(target_num_instances - len(running_transcribers)):
             # Find an offer
             offers_json = vast.search_offers(storage=DISK_GB, order="dph_total", query='cpu_cores_effective>=10 cpu_ram>=32 gpu_total_ram>=8 reliability>=0.95 dph<2 total_flops>=10')
             cheapest = json.loads(offers_json)[0]
@@ -51,7 +61,7 @@ def start_transcribe(event: pubsub_fn.CloudEvent[pubsub_fn.MessagePublishedData]
                 image="awongdev/transcribe:0.4",
                 label=INSTANCE_LABEL,
                 # Kill the server after 30 mins if it's still running.
-                onstart_cmd=f"env | grep _ >> /etc/environment; nohup /workspace/app/onstart_hook.sh {int(cheapest['cpu_cores_effective'])} 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 30 10 &",
+                onstart_cmd=f"env | grep _ >> /etc/environment; nohup /workspace/app/onstart_hook.sh {int(cheapest['cpu_cores_effective'])} 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 {lysine_timeout} 10 &",
                 disk=DISK_GB,
                 args="",
                 cancel_unavail=True,
