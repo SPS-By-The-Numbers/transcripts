3f756cb10fc6d9b5403a9e3112535b3e0c45111d
Albert J. Wong <awong.dev@gmail.com>
2024-05-30 23:44:57 -0700

    Add in manual video enqueue page

--- components/SpeakerInfoControl.tsx
+++ components/SpeakerInfoControl.tsx
@@ -114,7 +114,7 @@ export default function SpeakerInfoControl({category, className, speakerNums, vi
       const errorCode = error.code;
       const errorMessage = error.message;
       // The email of the user's account used.
-      const email = error.customData.email;
+      const email = error.customData?.email;
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
       console.error("Signin failed", errorCode, errorMessage, email, credential);
