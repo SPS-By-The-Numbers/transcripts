7c2e29673a0f8fb42427f453b4d05feca9f3457a
Mark Verrey <mverrey@gmail.com>
2024-06-06 23:56:36 -0700

    Prepopulate filter controls from URL parameters

---
+++ app/Transcripts.tsx
@@ -0,0 +1,126 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation.js';
+import Link from 'next/link';
+import { TailSpin } from 'react-loader-spinner';
+import resolveConfig from 'tailwindcss/resolveConfig';
+import { content, theme } from '../tailwind.config.js';
+import { VideoData, getAllVideosForDateRange } from '../utilities/metadata-utils';
+import { formatDateForPath, getVideoPath, parseDateFromPath } from 'utilities/path-utils';
+import TranscriptFilter, { TranscriptFilterSelection, DateRange } from 'components/TranscriptFilter';
+
+function useFilterParameters(allCategories: string[]): TranscriptFilterSelection {
+  const searchParams: URLSearchParams = useSearchParams();
+
+  const categoryParam: string | null = searchParams.get('category');
+
+  let category: string;
+
+  if (categoryParam !== null && allCategories.includes(categoryParam)) {
+    category = categoryParam;
+  }
+  else {
+    category = 'sps-board';
+  }
+
+  let start: Date | null = null;
+  const startParam: string | null = searchParams.get('start');
+
+  if (startParam !== null) {
+    const parsedStart: Date | undefined = parseDateFromPath(startParam);
+    if (parsedStart !== undefined) {
+      start = parsedStart;
+    }
+  }
+
+  let end: Date | null = null;
+  const endParam: string | null = searchParams.get('end');
+
+  if (endParam !== null) {
+    const parsedEnd: Date | undefined = parseDateFromPath(endParam);
+    if (parsedEnd !== undefined) {
+      end = parsedEnd;
+    }
+  }
+
+  let dateRange: DateRange;
+
+  if (start === null && end === null) {
+    dateRange = { start: parseDateFromPath('2024-03-01'), end: null };
+  }
+  else {
+    dateRange = { start, end };
+  }
+
+  return { category, dateRange };
+}
+
+export default function Transcripts({ allCategories }: { allCategories: string[] }) {
+  const initialFilters = useFilterParameters(allCategories);
+  const [filters, updateFilters]: [TranscriptFilterSelection, any] = useState(initialFilters);
+
+  const [isLoading, setLoading] = useState(true);
+  const [videos, setVideos]: [VideoData[], any] = useState([]);
+
+  useEffect(() => {
+    if (filters.category === null || (filters.dateRange.start === null && filters.dateRange.end === null)) {
+      setVideos([]);
+      return () => {};
+    }
+
+    let ignore = false;
+
+    const startDateString = filters.dateRange.start !== null ? formatDateForPath(filters.dateRange.start) : null;
+    const endDateString = filters.dateRange.end !== null ? formatDateForPath(filters.dateRange.end) : null;
+
+    getAllVideosForDateRange(filters.category, startDateString, endDateString)
+      .then(videoData => {
+        if (!ignore) {
+          setLoading(false);
+          setVideos(videoData);
+        }
+      });
+
+      return () => {
+        ignore = true;
+      }
+  }, [filters])
+
+  function handleFilterChange(filters: TranscriptFilterSelection) {
+    updateFilters(filters);
+  }
+
+  const videoLinks: React.ReactNode[] = videos.map(
+    video => (
+      <li key={video.videoId} className="mx-3 list-disc">
+        <Link href={getVideoPath(filters.category, video.videoId)}>
+          {video.title}
+        </Link>
+      </li>
+    ));
+
+  // Get access to the tailwind theme colors to pass to the loading spinner
+  // Specifying only content and theme keys is a workaround for a type error pertaining
+  // to the "future" key
+  const fullConfig = resolveConfig({ content, theme });
+  const loadingSection = <section className="my-4 flex flex-row">
+    <TailSpin wrapperClass="justify-center" color={fullConfig.theme.colors.blue['500']} />
+  </section>;
+
+  const resultsSection: React.ReactNode = <section>
+    <h2 className="my-4 text-lg">
+      Transcripts:
+    </h2>
+    <ul className="flex flex-col flex-wrap h-screen">
+      {videoLinks}
+    </ul>
+  </section>;
+
+  return (
+    <>
+      <TranscriptFilter selection={filters} onFilterChange={handleFilterChange} />
+      {isLoading ? loadingSection : resultsSection}
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,11 @@
-'use client'
+import { getAllCategories } from 'utilities/metadata-utils';
+import Transcripts from './Transcripts';
 
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { TailSpin } from 'react-loader-spinner';
-import resolveConfig from 'tailwindcss/resolveConfig';
-import { content, theme } from '../tailwind.config.js';
-import { VideoData, getAllVideosForDateRange } from '../utilities/metadata-utils';
-import { formatDateForPath, getVideoPath, parseDateFromPath } from 'utilities/path-utils';
-import TranscriptFilter, { TranscriptFilterSelection } from 'components/TranscriptFilter';
-
-export default function Index() {
-  const [filters, updateFilters]: [TranscriptFilterSelection, any] = useState(
-    {
-      category: 'sps-board',
-      dateRange: { start: parseDateFromPath('2024-03-01'), end: null }
-    });
-
-  function handleFilterChange(filters: TranscriptFilterSelection) {
-    updateFilters(filters);
-  }
-
-  const [isLoading, setLoading] = useState(true);
-  const [videos, setVideos]: [VideoData[], any] = useState([]);
-
-  useEffect(() => {
-    if (filters.category === null || (filters.dateRange.start === null && filters.dateRange.end === null)) {
-      setVideos([]);
-      return () => {};
-    }
-
-    let ignore = false;
-
-    const startDateString = filters.dateRange.start !== null ? formatDateForPath(filters.dateRange.start) : null;
-    const endDateString = filters.dateRange.end !== null ? formatDateForPath(filters.dateRange.end) : null;
-
-    getAllVideosForDateRange(filters.category, startDateString, endDateString)
-      .then(videoData => {
-        if (!ignore) {
-          setLoading(false);
-          setVideos(videoData);
-        }
-      });
-
-      return () => {
-        ignore = true;
-      }
-  }, [filters])
-
-  const videoLinks: React.ReactNode[] = videos.map(
-    video => (
-      <li key={video.videoId} className="mx-3 list-disc">
-        <Link href={getVideoPath(filters.category, video.videoId)}>
-          {video.title}
-        </Link>
-      </li>
-    ));
-
-  // Get access to the tailwind theme colors to pass to the loading spinner
-  // Specifying only content and theme keys is a workaround for a type error pertaining
-  // to the "future" key
-  const fullConfig = resolveConfig({ content, theme });
-  const loadingSection = <section className="my-4 flex flex-row">
-    <TailSpin wrapperClass="justify-center" color={fullConfig.theme.colors.blue['500']} />
-  </section>;
-
-  const resultsSection: React.ReactNode = <section>
-    <h2 className="my-4 text-lg">
-      Transcripts:
-    </h2>
-    <ul className="flex flex-col flex-wrap h-screen">
-      {videoLinks}
-    </ul>
-  </section>;
-
+export default async function Index() {
+  const allCategories: string[] = await getAllCategories();
   return (
     <main className="mx-5 my-5 max-w-screen-md">
-      <TranscriptFilter selection={filters} onFilterChange={handleFilterChange} />
-      {isLoading ? loadingSection : resultsSection}
+      <Transcripts allCategories={allCategories} />
     </main>
   );
 }
