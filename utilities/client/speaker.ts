a6f69464f7a855ba2968e13ed9e13dfdce3ba166
Albert J. Wong <awong.dev@gmail.com>
2024-06-07 00:10:44 -0700

    Allow multi-linguaal transcripts

---
+++ utilities/client/speaker.ts
@@ -0,0 +1,45 @@
+import { getExistingRef, getVideoRef } from "utilities/metadata-utils"
+import type { SpeakerInfoData } from 'utilities/speaker-info'
+
+type SpeakerControlInfo = {
+  speakerInfo: SpeakerInfoData,
+  existingNames: object,
+  existingTags: Set<string>,
+};
+
+type DbInfoEntry ={
+  name : string;
+  tags : Array<string>;
+};
+
+export async function loadSpeakerControlInfo(category: string, videoId: string) : Promise<SpeakerControlInfo> {
+  const videoRef = await getVideoRef(category, videoId);
+  const existingRef = await getExistingRef(category);
+
+  const speakerInfo = {};
+  const videoData = videoRef.val();
+  if (videoData && videoData.speakerInfo) {
+    for (const [k,v] of Object.entries(videoData.speakerInfo)) {
+      const entry = v as DbInfoEntry;
+      const n = entry?.name;
+      const t = entry?.tags;
+      speakerInfo[k] = speakerInfo[k] || {};
+      if (n && speakerInfo[k].name === undefined) {
+        speakerInfo[k].name = n;
+      }
+      if (t && speakerInfo[k].tags === undefined) {
+        speakerInfo[k].tags = new Set<string>(t);
+      }
+    }
+  }
+
+  const existingOptions = existingRef.val();
+
+  const existingNames = {...existingOptions?.names};
+  const existingTags = new Set<string>();
+  if (existingOptions?.tags) {
+    Object.keys(existingOptions.tags).forEach(tag => existingTags.add(tag));
+  }
+
+  return {speakerInfo, existingNames, existingTags};
+}
