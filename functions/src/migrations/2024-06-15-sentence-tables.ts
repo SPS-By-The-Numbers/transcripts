c34f099ca47dd0beac607cfb6d92f686330763e0
Albert J. Wong <awong.dev@gmail.com>
2024-06-29 21:56:36 -0700

    Revert "Deployable again."
    This reverts commit 56efc20eb5ee0ff68536ab65bd9e4248b48730fc.

--- functions/src/migrations/2024-06-15-sentence-tables.ts
+++ functions/src/migrations/2024-06-15-sentence-tables.ts
@@ -16,43 +16,39 @@ import { makePublicPath } from '../../../common/paths.js';
 
 import type { Iso6393Code, VideoId } from "../../../common/params.js";
 
-async function main() {
-  const accessor = new FirebaseAdminStorageAccessor();
+const accessor = new FirebaseAdminStorageAccessor();
 
-  for (const category of Constants.ALL_CATEGORIES) {
-    const [allFiles] = await accessor.bucket.getFiles(
-        { prefix: makePublicPath(category, Constants.WHISPERX_ARCHIVE_SUBDIR),
-          autoPaginate: true,
-          });
+for (const category of Constants.ALL_CATEGORIES) {
+  const [allFiles] = await accessor.bucket.getFiles(
+      { prefix: makePublicPath(category, Constants.WHISPERX_ARCHIVE_SUBDIR),
+        autoPaginate: true,
+        });
 
-    for (const file of allFiles) {
-      try {
-        const splits = basename(file.name).split('.')[0];
-        const vid : VideoId = splits[0];
-        const language : Iso6393Code = splits[1];
+  for (const file of allFiles) {
+    try {
+      const splits = basename(file.name).split('.')[0];
+      const vid : VideoId = splits[0];
+      const language : Iso6393Code = splits[1];
 
-        // Generate the filenames.
-        const diarizedPath = makeTranscriptDataPath(category, vid);
-        const sentencesPath = makeSentenceTablePath(category, vid, language);
-        const diarizedTranscriptExists = (await accessor.bucket.file(diarizedPath).exists())[0];
-        const sentenceTableExists = (await accessor.bucket.file(sentencesPath).exists())[0];
-        
-        if (!diarizedTranscriptExists || !sentenceTableExists) {
-          const diarizedTranscript = await DiarizedTranscript.fromWhisperXArchive(accessor, category, vid, language);
+      // Generate the filenames.
+      const diarizedPath = makeTranscriptDataPath(category, vid);
+      const sentencesPath = makeSentenceTablePath(category, vid, language);
+      const diarizedTranscriptExists = (await accessor.bucket.file(diarizedPath).exists())[0];
+      const sentenceTableExists = (await accessor.bucket.file(sentencesPath).exists())[0];
+      
+      if (!diarizedTranscriptExists || !sentenceTableExists) {
+        const diarizedTranscript = await DiarizedTranscript.fromWhisperXArchive(accessor, category, vid, language);
 
-          if (!sentenceTableExists) {
-            diarizedTranscript.writeSentenceTable(accessor, diarizedTranscript.originalLanguage);
-          }
-
-          if (!diarizedTranscriptExists) {
-            await diarizedTranscript.writeDiarizedTranscript(accessor);
-          }
+        if (!sentenceTableExists) {
+          diarizedTranscript.writeSentenceTable(accessor, diarizedTranscript.originalLanguage);
         }
-      } catch (e) {
-        console.error("Failed ", file.name, " with error ", e);
+
+        if (!diarizedTranscriptExists) {
+          await diarizedTranscript.writeDiarizedTranscript(accessor);
+        }
       }
+    } catch (e) {
+      console.error("Failed ", file.name, " with error ", e);
     }
   }
 }
-
-main()
