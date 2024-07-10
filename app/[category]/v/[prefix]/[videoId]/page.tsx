6cf2e7dd2c312f955eab2e43526b5cc2629f4bc5
Albert J. Wong <awong.dev@gmail.com>
2024-07-09 23:48:53 -0700

    Simplify the bare language redirect

--- app/[category]/v/[prefix]/[videoId]/page.tsx
+++ app/[category]/v/[prefix]/[videoId]/page.tsx
@@ -1,11 +1,8 @@
-export { generateMetadata } from './[lang]/page';
 import IndexWithLang from './[lang]/page';
 
 import type { VideoParams } from './[lang]/page';
 
-// Force video pages to be statically rendered.
-export const dynamic = 'force-static';
-export const revalidate = 600;
+export { generateMetadata, dynamic, revalidate } from './[lang]/page';
 
 export default async function Index({params} : {params: VideoParams}) {
   return IndexWithLang({params: {...params, lang: 'eng'}});
