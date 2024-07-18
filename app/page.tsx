04bd7907abcb6ed5ba95ecd50b086c94bb46f4dc
Albert J. Wong <awong.dev@gmail.com>
2024-07-18 00:36:42 -0700

    Move TranscriptIndex into categories, and search into root. CODE BROKEN

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,221 @@
+'use client'
+
 import * as Constants from 'config/constants';
-import TranscriptIndex from 'components/TranscriptIndex';
-import { encodeDate, decodeDate } from 'common/params';
-import { fetchEndpoint } from 'utilities/client/endpoint';
+import Link from 'next/link';
+import { MeiliSearch } from 'meilisearch';
+import { getVideoPath } from 'common/paths';
 import { parseISO } from 'date-fns';
+import { toHhmmss } from 'utilities/client/css';
+import { useState } from 'react';
 
-import type { DateRange } from 'components/TranscriptIndexFilter';
-
-const mostRecentLimit = 50;
+import type { ReactNode } from 'react';
 
 type SearchParams = {
-  [key: string]: string | string[] | undefined;
+    category: string,
 };
 
-export default async function Index({ searchParams } : { searchParams?: SearchParams }) {
+type Position = {
+  length: number;
+  start: number;
+};
 
-  const category = (searchParams?.category && typeof searchParams.category === 'string') ? searchParams.category : Constants.DEFAULT_CATEGORY;
-  const parameters : Record<string, string> = { category };
-  const range : DateRange = { start: null, end: null };
+type MatchesPosition = {
+  text: Array<Position>;
+};
 
-  // Extract dates. Reencode before sending to metadata endpoint to sanitize.
-  if (searchParams?.start && typeof(searchParams.start) === 'string') {
-    range.start = decodeDate(searchParams.start);
-    parameters['start'] = encodeDate(range.start);
-  }
+type ResultAttributes = {
+  videoId: string;
+  start: number;
+  publishDate: string;
+  title: string;
+  _formatted: {
+    id: string;
+    text: string;
+    videoId: string;
+    start: string;
+    end: string;
+    publishDate: string;
+    title: string;
+    transcribDate: string;
+  };
+  _matchesPosition: MatchesPosition;
+};
 
-  if (searchParams?.end && typeof(searchParams.end) === 'string') {
-    range.end = decodeDate(searchParams.end);
-    parameters['end'] = encodeDate(range.end);
-  }
+type SearchResults = {
+  hits: Array<ResultAttributes>,
+  query: string;
+  processingTimeMs: number;
+  limit: number;
+  offset: number;
+  estimatedTotalHits?: number;
+  totalHits?: number;
+};
 
-  // If no start or end, grab the most recent videos to RecentLimit
-  if (!searchParams?.start && !searchParams?.end) {
-    parameters.limit = mostRecentLimit.toString();
-  }
+type ResultParams = {
+    category: string,
+    results?: SearchResults,
+};
 
-  const response = await fetchEndpoint('metadata', 'GET', parameters);
-  let videos : Array<any> = [];
-  let banner = <></>;
-  if (response.ok && response.data.length > 0) {
-    videos = response.data.map(v => ({
-        videoId: v.videoId,
-        title: v.title,
-        publishDate: v.publishDate}));
-    // No dates huh? Set the start to oldest video.
-    if (parameters.limit) {
-      banner = (<p className="my-3">Showing first {mostRecentLimit} videos. Change Start Date to find older ones.</p>);
-      range.start = parseISO(videos[0].publishDate);
+const client = new MeiliSearch({
+  host: Constants.MEILI_ENDPOINT,
+  apiKey: Constants.MEILI_KEY,
+});
+
+function formatDate(dateStr : string) {
+  const date = parseISO(dateStr);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+function isValidSort(sortType: string) {
+  return (
+      sortType === 'publishDate:asc'
+      || sortType === 'publishDate:desc');
+}
+
+function doSearch(category : string, query : string, sortType : string, groupType: string, setResults) {
+  const indexName = `${category}-${Constants.CATEGORY_CHANNEL_MAP[category].language}`;
+  const index = client.index(indexName);
+
+  const sort = isValidSort(sortType) ? [ sortType ] : undefined;
+  const queryOptions = {
+      attributesToSearchOn: ['text'],
+      attributesToRetrieve: ['videoId', 'start', 'publishDate', 'title'],
+      attributesToCrop: [ 'text' ],
+      showMatchesPosition: true,
+      sort,
+      cropLength: 256,
+      hitsPerPage: 1000, // Force exact for now.
+      limit: 1000,
+    };
+  if (groupType === 'speaker') {
+    queryOptions['distinct'] = 'id';  // Make meilisearch return all documents individually.
+
+    // But sort them according to video and time.
+    const groupSort = ['videoId:asc', 'start:asc'];
+    if (queryOptions.sort) {
+      queryOptions.sort.push(...groupSort);
+    } else {
+      queryOptions.sort = groupSort;
     }
+  }
 
+  index.search(query, queryOptions)
+    .then((result : any) => {
+      console.log(result);
+      setResults(result);
+    });
+}
+
+function Snippet({ text, matchesPosition }: { text: string, matchesPosition: MatchesPosition } ) {
+  const textSegments : Array<ReactNode> = [];
+  let lastStart = 0;
+  for (const [i, match] of Object.entries(matchesPosition.text)) {
+    // Unmatched part.
+    if (lastStart !== match.start) {
+      textSegments.push(<span key={`${i}-no`}>{text.slice(lastStart, match.start)}</span>);
+    }
+    textSegments.push(<span className="bg-yellow-300 font-semibold" key={`${i}-em`}>{text.slice(match.start, match.start + match.length)}</span>);
+    lastStart = match.start + match.length;
+  }
+  if (lastStart !== text.length -1) {
+    textSegments.push(<span key='last'>{text.slice(lastStart)}</span>);
   }
 
   return (
-    <>
-      <main className="mx-5 my-5 max-w-screen-md">
-        { banner }
-        <TranscriptIndex category={category} videos={videos} range={range}/>
-      </main>
-    </>
+   <div>
+     { textSegments }
+   </div>
   );
+}
+
+function ResultList({ category, results }: ResultParams) {
+  if (!results) {
+    return (
+      <section>
+        <p className="text-lg">Do a search!</p>
+        <p className="text-sm">(The first search may be really slow...like 2-3 mins...cause the engine needs to warm up)</p>
+      </section>);
+  }
+
+  const hits = results.hits;
+  if (hits.length === 0) {
+    return (<section>No Results</section>);
+  }
+
+  const resultElements = hits.map((h,i) => {
+    return (
+      <article key={i} className="mx-3 mb-2 bg-gray-300 p-2 rounded-lg">
+        <p>
+          <Link className="text-lg font-medium" href={`${getVideoPath(category, h.videoId)}#${toHhmmss(h.start)}`}>
+          {h.title}
+          </Link>
+          <br />
+          <span className="text-sm"><b>Published:</b> {formatDate(h.publishDate)}, <b>VideoId:</b> {h.videoId}, <b>Time:</b> {toHhmmss(h.start)}</span>
+        </p>
+        <p>
+        <Snippet text={h._formatted.text} matchesPosition={h._matchesPosition} />
+        </p>
+      </article>
+    );
+  });
+
+  return (
+      <section>
+        <div className="p-2 m-3 bg-gray-400 text-xs rounded-lg">
+         <b>Query:</b> {results.query}<br/>
+         <b>Results:</b> {results.estimatedTotalHits || results.totalHits}<br/>
+         <b>Processing Time:</b> {results.processingTimeMs}ms<br/>
+        </div>
+        { resultElements }
+      </section>);
+}
+
+export default function Search({ params }: { params: SearchParams }) {
+  const [query, setQuery] = useState<string>("");
+  const [sortType, setSortType] = useState<string>("relevance");
+  const [groupType, setGroupType] = useState<string>("video");
+  const [results, setResults] = useState<SearchResults | undefined>();
+
+  return (
+      <div className="p-2">
+        <form
+            className="flex items-center justify-center"
+            onSubmit={(e) => {
+              e.preventDefault();
+              doSearch(params.category, query, sortType, groupType, setResults);
+            }}>
+          <label> Result Grouping: 
+          <select onChange={(e) => setGroupType(e.target.value)} className="m-2 border-black border-2" value={groupType}>
+            <option value="video">By Video</option>
+            <option value="speaker">By Speaker Turn</option>
+          </select>
+          </label>
+          <label> Result Sort: 
+          <select onChange={(e) => setSortType(e.target.value)} className="m-2 border-black border-2" value={sortType}>
+            <option value="relevance">Relevance</option>
+            <option value="publishDate:asc">Publish Date (asc)</option>
+            <option value="publishDate:desc">Publish Date (desc)</option>
+          </select>
+          </label>
+          <input
+              className="rounded border-black border-2 mx-1 px-2 py-2 w-4/12 max-w-screen-sm"
+              type="text"
+              placeholder="query here"
+              value={query}
+              onChange={e => setQuery(e.target.value)} />
+          <button
+              type="submit"
+              className="border-2 px-6 py-2 mx-1 bg-sky-500 rounded">
+            Search
+          </button>
+        </form>
+        <hr className="my-2"/>
+        <div>
+          <br />
+          <ResultList category={params.category} results={results} />
+        </div>
+      </div>);
 }
