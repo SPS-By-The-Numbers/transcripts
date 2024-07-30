9ea4d58949796c9923a6beceec1c7d2e1a1e7eab
Albert J. Wong <awong.dev@gmail.com>
2024-07-30 00:30:00 -0700

    Delete old script from static-file days

--- syncdata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcloud storage rsync --recursive --checksums-only --exclude '.*\.mp4|.*\.txt|.*\.vtt|.*\.tsv|.*\.srt' gs://sps-by-the-numbers.appspot.com/transcription ./data/transcripts
