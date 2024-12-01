2ae3f086bd984348a94713c8f0cf253eded77815
Albert J. Wong <awong.dev@gmail.com>
2024-11-30 23:29:07 -0800

    Fix type errors upgrading to Next 15

--- app/[category]/v/[videoId]/page.tsx
+++ app/[category]/v/[videoId]/page.tsx
@@ -2,10 +2,9 @@ import IndexWithLang from './[lang]/page';
 
 import type { VideoParams } from './[lang]/page';
 
-export { /* @next-codemod-error `generateMetadata` export is re-exported. Check if this component uses `params` or `searchParams`*/
-generateMetadata, dynamic, revalidate } from './[lang]/page';
+export { generateMetadata, dynamic, revalidate } from './[lang]/page';
 
 export default async function Index(props: {params: Promise<VideoParams>}) {
   const params = await props.params;
-  return IndexWithLang({params: {...params, lang: 'eng'}});
+  return IndexWithLang({params: Promise.resolve({...params, lang: 'eng'})});
 }
