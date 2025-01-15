2a3d8229682f01e52c83cc8788627daea722b764
Albert J. Wong <awong.dev@gmail.com>
2025-01-14 22:32:38 -0800

    Clean up date search navigation and move to Archive

--- app/[category]/v/page.tsx
+++ app/[category]/v/page.tsx
@@ -1,12 +1,12 @@
 import * as Constants from 'config/constants';
 import Alert from '@mui/material/Alert';
+import Archive from 'components/Archive';
 import Stack from '@mui/material/Stack';
-import DateSearchIndex from 'components/DateSearchIndex';
 import { encodeDate, decodeDate } from 'common/params';
 import { fetchEndpoint } from 'utilities/client/endpoint';
 import { parseISO } from 'date-fns';
 
-import type { DateRange } from 'components/DateSearchIndexFilter';
+import type { DateRange } from 'components/DateRangePicker';
 
 const mostRecentLimit = 50;
 
@@ -14,32 +14,47 @@ type SearchParams = {
   [key: string]: string | string[] | undefined;
 };
 
+async function doDateSearch(category : CategoryId, range : DateRange) {
+  const metadataParams : Record<string, string> = { category };
+  if (range.start !== null) {
+    const encoded = encodeDate(range.start, true);
+    if (encoded) {
+      metadataParams['start'] = encoded;
+    }
+  }
+  if (range.end !== null) {
+    const encoded = encodeDate(range.end, true);
+    if (encoded) {
+      metadataParams['end'] = encoded;
+    }
+  }
+
+  if (metadataParams['start'] && metadataParams['end']) {
+      metadataParams['limit'] = mostRecentLimit.toString();
+  }
+
+  return await fetchEndpoint('metadata', 'GET', metadataParams);
+}
+
 export default async function ArchiveIndex(
     props: { params: Promise<{category: CategoryId}>, searchParams?: Promise<SearchParams> }) {
   const searchParams = await props.searchParams;
   const params = (await props.params);
 
   const category = params.category;
-  const parameters : Record<string, string> = { category };
+  const metadataParams : Record<string, string> = { category };
   const range : DateRange = { start: null, end: null };
 
   // Extract dates. Reencode before sending to metadata endpoint to sanitize.
   if (searchParams?.start && typeof(searchParams.start) === 'string') {
     range.start = decodeDate(searchParams.start);
-    parameters['start'] = encodeDate(range.start);
   }
 
   if (searchParams?.end && typeof(searchParams.end) === 'string') {
     range.end = decodeDate(searchParams.end);
-    parameters['end'] = encodeDate(range.end);
   }
 
-  // If no start or end, grab the most recent videos to RecentLimit
-  if (!searchParams?.start && !searchParams?.end) {
-    parameters.limit = mostRecentLimit.toString();
-  }
-
-  const response = await fetchEndpoint('metadata', 'GET', parameters);
+  const response = await doDateSearch(category, range);
   let videos = new Array<any>();
   let showBanner = false;
   if (response.ok && response.data.length > 0) {
@@ -47,12 +62,6 @@ export default async function ArchiveIndex(
         videoId: v.videoId,
         title: v.title,
         publishDate: v.publishDate}));
-  }
-
-  // No dates huh? Set the start to oldest video.
-  if (parameters.limit) {
-    showBanner = true;
-    range.start = parseISO(videos?.[0].publishDate);
   }
 
   return (
@@ -63,10 +72,18 @@ export default async function ArchiveIndex(
           padding: "1ex",
         }}>
       <Alert variant="filled" severity="info"
-        sx={{display: showBanner ? "flex" : "none" }}>
-        Showing first {mostRecentLimit} videos. Change Start Date to find older ones.
+        sx={{display: !range.start && !range.end ? "flex" : "none" }}>
+        Showing first {mostRecentLimit} videos. Change the Start Date to find older ones.
       </Alert>
-      <DateSearchIndex category={category} videos={videos} range={range}/>
+      <Alert variant="filled" severity="error"
+        sx={{display: response.ok ? "none" : "flex" }}>
+        {response.message}
+      </Alert>
+      <Archive
+        category={category}
+        dateRange={range}
+        videos={videos}
+      />
     </Stack>
   );
 }
