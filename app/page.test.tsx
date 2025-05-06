be26c6209edcbc1c34ca8b3d96dc2a765939d949
Mark Verrey <mverrey@gmail.com>
2025-05-06 16:54:48 -0700

    Add tests for transcripts landing page
    Mainly a proof of concept for page-level unit testing and how to use
    Testing Library

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,38 +1,37 @@
+import { getAllByRole, getByRole, queryByRole, render, screen, RenderResult } from '@testing-library/react';
 import Landing from './page';
-import { render, screen, RenderResult } from '@testing-library/react';
+import type { ApiResponse } from 'common/response';
 import * as Constants from 'config/constants';
 import { fetchEndpoint } from 'utilities/client/endpoint';
 
 jest.mock('utilities/client/endpoint');
 
-const dummyMetadata = [{
+const dummyVideo = {
   channelId: 'UC07MVxpRKdDJmqwWDGYqotA',
   description: 'Seattle Public Schools',
   publishDate: '2015-11-04T21:05:52-08:00',
   title: 'School Board Meeting Date November 4th, 2015 Pt.2',
   videoId: '-95KMDHf4vQ'
-}, {
-  channelId: 'UC07MVxpRKdDJmqwWDGYqotA',
-  description: 'Seattle Public Schools',
-  publishDate: '2024-10-09T22:02:50-07:00',
-  title: 'Seattle Schools Board Meeting Oct. 9, 2024',
-  videoId: '-MyMd2xOYdw'
-}, {
-  channelId: 'UC07MVxpRKdDJmqwWDGYqotA',
-  description: 'Seattle Public Schools',
-  publishDate: '2017-01-20T15:21:53-08:00',
-  title: 'School Board Meeting 1 18 2017 Part 2',
-  videoId: '-PMPKQzkJYg'
-}];
+};
 
-// const dummyCategories = Constants.ALL_CATEGORIES.map(category => ({
-//   [category]: {...dummyMetadata}
-// }));
-
-const dummyResponse = {
-  ok: true,
-  message: 'success',
-  data: [...dummyMetadata]
+const dummyMetadata = {
+  'sps-board': [{
+    ...dummyVideo,
+    videoId: 'video1'
+  }, {
+    ...dummyVideo,
+    videoId: 'video2'
+  }, {
+    ...dummyVideo,
+    videoId: 'video3'
+  }],
+  'seattle-city-council': [{
+    ...dummyVideo,
+    videoId: 'video1'
+  }, {
+    ...dummyVideo,
+    videoId: 'video2'
+  }]
 };
 
 async function renderServerFunctionComponent(
@@ -50,14 +49,121 @@ async function renderServerFunctionComponent(
   return render(reactNode);
 }
 
+function mockCategoryData(categoryMetadata: { [category: string]: any }) {
+  (fetchEndpoint as jest.MockedFunction<typeof fetchEndpoint>).mockImplementation(
+    (endpoint: string, method: string, parameters: object | Record<string, string>): Promise<ApiResponse> => {
+      const category: string = parameters['category'];
+
+      return Promise.resolve<ApiResponse>({ ok: true, message: 'success', data: [ ...categoryMetadata[category] ] });
+    });
+}
+
+function mockError(errorMessage: string) {
+  (fetchEndpoint as jest.MockedFunction<typeof fetchEndpoint>).mockImplementation(
+    (endpoint: string, method: string, parameters: object | Record<string, string>): Promise<ApiResponse> => {
+      return Promise.resolve<ApiResponse>({ ok: false, message: errorMessage, data: [] });
+    });
+}
+
+async function renderLandingPage(): Promise<RenderResult> {
+  const Page = await Landing();
+  return await renderServerFunctionComponent(Page);
+}
+
+function getAllCategoryHeaders(): HTMLElement[] {
+  return screen.getAllByRole('heading', { level: 3 });
+}
+
+function getRegionForCategory(categoryName: string): HTMLElement {
+  return screen.getByRole('region', { name: `${categoryName} - Recent Transcripts` });
+}
+
+function getRegionLinks(region: HTMLElement): HTMLElement[] {
+  return getAllByRole(region, 'link');
+}
+
+function getLinkHeading(link: HTMLElement): HTMLElement {
+  return getByRole(link, 'heading');
+}
+
 describe('Transcripts index page', () => {
-  beforeEach(() => {
-    (fetchEndpoint as jest.MockedFunction<typeof fetchEndpoint>).mockResolvedValue({...dummyResponse});
+  it('shows both categories', async () => {
+    mockCategoryData(dummyMetadata);
+    await renderLandingPage();
+    const headers = getAllCategoryHeaders();
+    expect(headers.length).toBe(2);
   });
 
-  it('renders', async () => {
-    const Page = await Landing();
-    await renderServerFunctionComponent(Page);
-    expect(screen.queryAllByRole('heading').length).toBeGreaterThan(0);
+  it('shows different article lists for different categories', async () => {
+    const firstBoardMeetingTitle = 'First Board Meeting';
+
+    const spsBoardData = [{
+      ...dummyVideo,
+      title: firstBoardMeetingTitle,
+      videoId: 'video1',
+    }, {
+      ...dummyVideo,
+      videoId: 'video2'
+    }];
+
+    const firstCouncilMeetingTitle = 'First Council Meeting';
+
+    const cityCouncilData = [{
+      ...dummyVideo,
+      title: firstCouncilMeetingTitle,
+      videoId: 'video3'
+    }, {
+      ...dummyVideo,
+      videoId: 'video4'
+    }, {
+      ...dummyVideo,
+      videoId: 'video5'
+    }];
+
+    mockCategoryData({
+      ...dummyMetadata,
+      'sps-board': spsBoardData,
+      'seattle-city-council': cityCouncilData,
+    });
+
+    await renderLandingPage();
+
+    const spsBoardRegion: HTMLElement = getRegionForCategory('SPS Board');
+    const spsBoardLinks: HTMLElement[] = getRegionLinks(spsBoardRegion);
+
+    expect(spsBoardLinks.length).toBe(2);
+    const firstBoardLinkTitle: HTMLElement = getLinkHeading(spsBoardLinks[0]);
+    expect(firstBoardLinkTitle.textContent).toBe(firstBoardMeetingTitle);
+
+    const cityCouncilRegion: HTMLElement = getRegionForCategory('Seattle City Council');
+    const cityCouncilLinks: HTMLElement[] = getRegionLinks(cityCouncilRegion);
+
+    expect(cityCouncilLinks.length).toBe(3);
+    const firstCouncilLinkTitle: HTMLElement = getLinkHeading(cityCouncilLinks[0]);
+    expect(firstCouncilLinkTitle.textContent).toBe(firstCouncilMeetingTitle);
+  });
+
+  it('shows a message when a category has no data', async () => {
+    mockCategoryData({
+      ...dummyMetadata,
+      'sps-board': []
+    });
+
+    await renderLandingPage();
+
+    const spsBoardRegion: HTMLElement = getRegionForCategory('SPS Board');
+    const messageElement: HTMLElement = getByRole(spsBoardRegion, 'alert');
+    expect(messageElement.textContent).toBe('Sadness. Nothing in this category.');
+  });
+
+  it('shows an error message when the API call fails', async () => {
+    const errorMessage = 'an error';
+    mockError(errorMessage);
+
+    await renderLandingPage();
+
+    const spsBoardRegion: HTMLElement = getRegionForCategory('SPS Board');
+    const messageElement: HTMLElement = getByRole(spsBoardRegion, 'alert');
+    expect(messageElement.textContent).toBe(`Unable to load recents: ${errorMessage}`);
   });
 });
