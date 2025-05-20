bc7bc64abd4a376278ed5055e30012d9703c1ae4
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2025-05-19 20:36:22 -0700

    Make curl silent

--- tools/search/recreate_index.sh
+++ tools/search/recreate_index.sh
@@ -20,13 +20,13 @@ nohup ${SEARCH_DIR}/meilisearch --db-path=$SEARCH_DB --master-key="$MASTER_KEY" 
 SEARCH_PID=$!
 
 # Wait until health check comes back with 200.
-until curl -X -f "http://127.0.0.1:7700/health"
+until curl --silent -X -f "http://127.0.0.1:7700/health"
 do
   sleep 1
 done
 
 # Register the key UID. The same (Master Key, UID) pair will generate the same key.
-until curl -X POST -H "Content-Type: application/json" --data-binary '{"name":"Transcript Front Search", "description":"Transcript Frontend Search Access", "uid": "fa82128d-a898-42ca-86eb-0e056195a111", "actions": ["search"], "indexes":["*"], "expiresAt": null}' -H "Authorization: Bearer $MASTER_KEY" -s -f -o /dev/null "http://127.0.0.1:7700/keys"
+until curl --silent -X POST -H "Content-Type: application/json" --data-binary '{"name":"Transcript Front Search", "description":"Transcript Frontend Search Access", "uid": "fa82128d-a898-42ca-86eb-0e056195a111", "actions": ["search"], "indexes":["*"], "expiresAt": null}' -H "Authorization: Bearer $MASTER_KEY" -s -f -o /dev/null "http://127.0.0.1:7700/keys"
 do
   sleep 1
 done
