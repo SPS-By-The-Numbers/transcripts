36bce0c6be4437d192eca7eb8ba1fa387a33aeb1
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2025-05-21 22:00:01 -0700

    Make Dockerfile.search work on Cloud Run (#120)
    This new image downloads Meilisearch's index files from Cloud Storage.

--- tools/search/start_meilisearch.sh
+++ tools/search/start_meilisearch.sh
@@ -1,6 +1,21 @@
-#! /bin/bash
+#! /bin/sh
 set -euo pipefail
 
-# TODO: download stuff from Google Cloud Storage to /meili_data/data.ms
+# gcp_key_path is for testing this image outside of Cloud Run.
+# On Cloud Run, permissions are granted via the service account of the job.
+gcp_key_path=/run/secrets/gcp.json
+if [[ -e "$gcp_key_path" ]]; then
+    gcloud auth activate-service-account "--key-file=$gcp_key_path"
+fi
 
-exec /bin/meilisearch --master-key=$(cat /run/secrets/meilisearch)
+# This key is for testing.
+# The key must be at least 16 bytes long.
+# https://www.meilisearch.com/docs/learn/security/basic_security#creating-the-master-key-in-a-self-hosted-instance
+MASTER_KEY='MEILI-9133dcf0-d6d9-4d24-a217-ae68fdb04475'
+
+# Download the index from Cloud Storage.
+mkdir -p "$SEARCH_DIR"
+gcloud storage cp --project "$GCP_PROJECT_ID" \
+    --recursive "gs://$GCP_BUCKET/data.ms" "$SEARCH_DIR"
+
+exec /bin/meilisearch "--db-path=$SEARCH_DB" "--master-key=$MASTER_KEY"
