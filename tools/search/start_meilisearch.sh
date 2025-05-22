f232040a38406f50dca76d9c30301499b09e4c41
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2025-05-22 08:12:34 -0700

    Set CLOUDSDK_AUTH_ACCESS_TOKEN explicitly

--- tools/search/start_meilisearch.sh
+++ tools/search/start_meilisearch.sh
@@ -4,8 +4,11 @@ set -euo pipefail
 # gcp_key_path is for testing this image outside of Cloud Run.
 # On Cloud Run, permissions are granted via the service account of the job.
 gcp_key_path=/run/secrets/gcp.json
-if [[ -e "$gcp_key_path" ]]; then
+if [ -e "$gcp_key_path" ]; then
     gcloud auth activate-service-account "--key-file=$gcp_key_path"
+else
+  export CLOUDSDK_AUTH_ACCESS_TOKEN=$(curl -s -H "Metadata-Flavor: Google" \
+    "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" | jq -r .access_token)
 fi
 
 # Download the index from Cloud Storage.
