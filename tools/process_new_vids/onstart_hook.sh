6e678af70b6545fa9ccb3501e0229de86a01ad20
Albert J. Wong <awong.dev@gmail.com>
2025-06-30 00:00:40 -0700

    Upgrade to whisper 3.4.2 and fix LD_LIBRARY_PATH issues

--- tools/process_new_vids/onstart_hook.sh
+++ tools/process_new_vids/onstart_hook.sh
@@ -5,5 +5,15 @@ export PATH=${HOME}/.local/share/fnm:$PATH
 eval "$(fnm env --use-on-cd --shell bash)"
 fnm use lts/latest
 
+cd /workspace/app
+
 # Default to giving 10 mins on failure.
-/workspace/app/lysine_protocol.sh "${4-:30}" & python /workspace/app/transcribe_worker.py -w /tmp/transcribe -t "${1:-4}" -x "$2" -y "$3" -m large-v3-turbo -c -s; /workspace/app/lysine_protocol.sh "${5:-10}"
+./lysine_protocol.sh "${4-:30}" &
+
+# Always get the latest pytubefix cause youtube is always changing.
+pip install pytubefix --upgrade
+
+# Do transcription
+python transcribe_worker.py -w /tmp/transcribe -t "${1:-4}" -x "$2" -y "$3" -m large-v3-turbo -c -s
+
+./lysine_protocol.sh "${5:-10}"
