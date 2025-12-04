97ae5e568fe8f5bd0aa5daad753a02f558db72f0
Albert J. Wong <awong.dev@gmail.com>
2025-12-03 22:01:07 -0800

    Fix eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([
+    globalIgnores(["tools/**/*", "functions/**/*", "functions-python/**/*", "node_modules/**/*", "coverage", ".*/**/*"]),
+    {
+        extends: compat.extends("next/core-web-vitals"),
+
+        rules: {
+            "@next/next/no-img-element": "off",
+            "@next/next/no-page-custom-font": "off",
+        },
+    },
+]);
